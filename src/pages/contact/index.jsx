fb2aa52967c6326b3e31ad73b44c20f7e2523406
Martin Mroz <mamr@hotmail.no>
2023-03-09 19:19:36 +0100

    added form label, and input requirements

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -7,29 +7,33 @@ const schema = yup
   .object({
     fullName: yup
       .string()
-      .min(4, "Your full name should be at least 4 characters.")
+      .min(3, "Your full name should be at least 3 characters.")
       .max(49, "Your full name cannot be longer than 49 characters.")
       .required("Please enter your first name"),
+  
+    subject: yup
+      .string()
+      .min(3, "Your subject should be at least 3 characters.")
+      .max(99, "Your full name cannot be longer than 99 characters.")
+      .required("Field required"),
+   
+   
     email: yup
       .string()
       .email()
-      .required("Please enter your email"),
+      .required("Please enter a valid email"),
 
-    subject: yup
-      .string()
-      .min(4, "Your subject should be at least 4 characters.")
-      .max(99, "Your full name cannot be longer than 99 characters.")
-      .required("Field required"),
+
 
     body: yup
       .string()
-      .min(4, "Your subject should be at least 4 characters.")
-      .max(999, "Your full name cannot be longer than 999 characters.")
+      .min(3, "Your subject should be at least 3 characters.")
+      .max(150, "Your full name cannot be longer than 150 characters.")
       .required("Field required"),
   })
   .required();
 
-function App() {
+function Form() {
   const {
     register,
     handleSubmit,
@@ -44,15 +48,19 @@ function App() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="">Full Name</label>
       <input name="fullName" type="text" {...register("fullName")} />
       <p>{errors.fullName?.message}</p>
-      
-      <input name="email" type="email" {...register("email")} />
-      <p>{errors.email?.message}</p>
 
+      <label htmlFor="">Subject</label>
       <input name="subject" type="text" {...register("subject")} />
       <p>{errors.subject?.message}</p>
 
+      <label htmlFor="">Email</label>
+      <input name="email" type="email" {...register("email")} />
+      <p>{errors.email?.message}</p>
+
+      <label htmlFor="">Body</label>
       <input name="body" type="text" {...register("body")} />
       <p>{errors.body?.message}</p>
 
@@ -61,4 +69,4 @@ function App() {
   );
 }
 
-export default App;
+export default Form;

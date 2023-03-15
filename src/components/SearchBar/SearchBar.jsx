35d85ba94379d0f83780fcba5fb29f1f94d90a5c
Martin Mroz <mamr@hotmail.no>
2023-03-15 11:21:22 +0100

    search list is filtering api data

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,39 +1,58 @@
+import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-// import SearchIcon from '@mui/icons-material/Search';
 import { useAPI } from "../../hooks/api";
-// import "./SearchBar.css"
 import { baseURL } from "../../utilities/constants";
 
 function AutocompleteSearchBar() {
+  let params = useParams();
+  const { data, isLoading, isError } = useAPI(baseURL);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const { data } = useAPI //(`${baseURL}products?search=${searchTerm}`);
-
-  console.log(data)
-
-/*  const handleInputChange = (event) => {
+  
+  const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
+  console.log(searchResults)
+  console.log(handleInputChange)
+
   useEffect(() => {
-    if (data && data.length) {
+    if (!isLoading && data && data.length) {
       const results = data.filter((item) =>
         item.title.toLowerCase().includes(searchTerm.toLowerCase())
       );
       setSearchResults(results);
+    } else {
+      setSearchResults([]);
     }
-  }, [data, searchTerm]); */
+  }, [data, searchTerm, isLoading]);
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-   { /*  <input type="text" value={searchTerm} onChange={handleInputChange} /> */ }
-      <ul>
-        {searchResults.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ul>
+      <input type="text" value={searchTerm} onChange={handleInputChange} />
+      {searchResults.length ? (
+        <ul>
+          {searchResults.map((item) => (
+            <li key={item.id}>{item.title}</li>
+          
+          ))}
+        </ul>
+      ) : (
+        
+        <div>No results found</div>
+        
+      )}
     </div>
+    
   );
 }
-
+   
 export default AutocompleteSearchBar;

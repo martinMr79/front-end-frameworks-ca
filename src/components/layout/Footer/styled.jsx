dfad19906c4b85b99b0aa7b2bdccadf58e710d55
Martin Mroz <mamr@hotmail.no>
2023-03-19 23:39:16 +0100

    small fix

--- src/components/layout/Footer/styled.jsx
+++ src/components/layout/Footer/styled.jsx
@@ -4,10 +4,12 @@ import styled from 'styled-components';
 export const FooterStyle = styled.footer`
 
 footer {
-    background-color: gray;
+    background-color: #311721; 
+    color: white;
     text-align: center;
     margin-top: auto;
     padding: 8px;
+    font-size: 1rem;
   }
 
   `

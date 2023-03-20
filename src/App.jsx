74955ddedba43e8209d86dc81504a88be8100eec
Martin Mroz <mamr@hotmail.no>
2023-03-20 13:48:54 +0100

    App

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+                                                                                                                                    import React from "react";
+import { Nav, Checkout, CheckoutSuccess } from "./components/layout/Header/Nav";
+import  Footer  from "./components/layout/Footer/index";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { Home, ProductPage, ContactPage } from "./pages"
+
+
+
+
+function App() {  
+
+return (   
+    <BrowserRouter>
+      <Nav />
+      
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="Contact" element={<ContactPage />} />
+        <Route path="product/:id" element={<ProductPage />} />        
+        <Route path="Checkout" element={<Checkout />} />
+        <Route path="CheckoutSuccess" element={<CheckoutSuccess />} />
+      </Routes>
+     <Footer/>
+    </BrowserRouter>
+    
+  );
+}
+export default App;

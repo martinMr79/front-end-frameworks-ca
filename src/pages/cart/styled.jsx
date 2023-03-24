67875642d1c37d43377a87573aaf355fe08ead91
Martin Mroz <mamr@hotmail.no>
2023-03-24 15:04:15 +0100

    styled clear cart button

--- src/pages/cart/styled.jsx
+++ src/pages/cart/styled.jsx
@@ -3,19 +3,33 @@ import styled from "styled-components"
 export const CartCount = styled.div `
 color: black;
 font-weight: bold;
-font-size: 16px;
+font-size: 20px;
 padding: 2px 5px;
 
 `; 
 
 export const CartItems = styled.div `
 color: black;
-font-size: 16px;
+
 padding: 2px 5px;
 
     li {
         text-decoration: none;
-        font-size: 16px;
+        font-size: 18px;
         list-style-type: none;
     }
 `; 
+
+export const ClearCartButton = styled.button`
+background-color: black;
+color: white;
+font-size: 20px;
+padding: 10px 60px;
+border-radius: 5px;
+margin: 10px 0px;
+cursor: pointer;
+ :hover {
+    background-color: white;
+    color: black;
+ }
+`
